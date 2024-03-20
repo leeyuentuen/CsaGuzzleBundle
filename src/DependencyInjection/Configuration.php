4279aa2f814d8c20be148d06f201d3ed9ed22557
Tuen Lee <leeyuentuen@gmail.com>
2024-03-20 15:52:19 +0100

    use return type treebuilder

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -29,7 +29,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritdoc}
      */
-    public function getConfigTreeBuilder() : void
+    public function getConfigTreeBuilder() : TreeBuilder
     {
         $treeBuilder = new TreeBuilder('csa_guzzle');
         if (method_exists($treeBuilder, 'getRootNode')) {
